40871168203a057bf4978def74795c4519ed02b8
Kirill Skorobogatov <Kirill9806@gmail.com>
2017-12-04 17:50:17 +0300

    Update test_main.py

--- Part 2/test_main.py
+++ Part 2/test_main.py
@@ -6,11 +6,11 @@ import main
 class TestEncoding(unittest.TestCase):
   @given(st.lists(st.integers()))
   def test_bubble_sort(self, xs):
-	result = sorted(xs)
+    result = sorted(xs)
     self.assertEqual(bubble_sort(xs), result) 
     
   @given(st.lists(st.integers()))
   def test_quicksort(self, xs):
-	result = sorted(xs)
+    result = sorted(xs)
     self.assertEqual(quicksort(xs), result)   
                                          
