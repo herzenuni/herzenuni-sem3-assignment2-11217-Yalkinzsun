d6df6507554fa61f8d16cad00670c7769b3b5a0a
Kirill Skorobogatov <Kirill9806@gmail.com>
2017-12-04 17:49:47 +0300

    Update test_main.py

--- Part 2/test_main.py
+++ Part 2/test_main.py
@@ -1,8 +1,16 @@
+import pytest
 import hypothesis.strategies as st
 from hypothesis import given
 import main
 
-@given(st.lists(st.integers()))
-def test_sort(xs):
-    result = sorted(xs)
-    assert all(xi <= xj for xi, xj in zip(result, result[1:]))
+class TestEncoding(unittest.TestCase):
+  @given(st.lists(st.integers()))
+  def test_bubble_sort(self, xs):
+	result = sorted(xs)
+    self.assertEqual(bubble_sort(xs), result) 
+    
+  @given(st.lists(st.integers()))
+  def test_quicksort(self, xs):
+	result = sorted(xs)
+    self.assertEqual(quicksort(xs), result)   
+                                         
